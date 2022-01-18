3266ace1c4d2e10ec8742f8a2de65eb4f4bd16ab
Dmitry Romanov <me@wtrn.ru>
2022-01-18 23:33:25 +0300

    Correct implementation of encryption/decryption

--- src/token.rs
+++ src/token.rs
@@ -5,48 +5,58 @@ use openssl::symm::Cipher;
 use std::ops::Deref;
 
 pub struct MiIoToken {
-    token_hash: DigestBytes,
+    token_bytes: Vec<u8>,
+    token_md5: DigestBytes,
     iv: DigestBytes,
 }
 
 impl MiIoToken {
-    pub fn new(token: &str) -> Result<MiIoToken, anyhow::Error> {
+    pub fn new(token_hex: &str) -> Result<MiIoToken, anyhow::Error> {
         let message_digest = openssl::hash::MessageDigest::md5();
-        let token_bytes = token.as_bytes();
-        let token_hash = openssl::hash::hash(message_digest, token_bytes)?;
+        let token_bytes = hex::decode(token_hex)?;
+        let token_md5 = openssl::hash::hash(message_digest, token_bytes.deref())?;
 
         let iv = {
-            let bytes = [token_hash.deref(), token_bytes.deref()].concat();
+            let bytes = [token_md5.deref(), token_bytes.deref()].concat();
             openssl::hash::hash(message_digest, bytes.deref())?
         };
 
-        Ok(MiIoToken { token_hash, iv })
+        Ok(MiIoToken { token_bytes, token_md5, iv })
     }
 
     pub fn encrypt(&self, msg: &[u8]) -> Result<Vec<u8>, anyhow::Error> {
         let cipher = Cipher::aes_128_cbc();
-        let encrypted = openssl::symm::encrypt(cipher, &self.token_hash, Some(&self.iv), msg)?;
+        let encrypted = openssl::symm::encrypt(cipher, &self.token_md5, Some(&self.iv), msg)?;
         Ok(encrypted)
     }
 
-    pub fn decrypt(&self, msg: &mut BytesMut) {}
+    pub fn decrypt(&self, msg: &[u8]) -> Result<Vec<u8>, anyhow::Error> {
+        let cipher = Cipher::aes_128_cbc();
+        let decrypted = openssl::symm::decrypt(cipher, &self.token_md5, Some(&self.iv), msg)?;
+        Ok(decrypted)
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::ops::Deref;
+    use serde_json::Value::String;
     use crate::token::MiIoToken;
 
     #[test]
     fn test_token_construction() {
-        let token = MiIoToken::new("test").unwrap();
+        let token_hex = "586e584268475142564d485234734d4b";
+        let token = MiIoToken::new(token_hex).unwrap();
 
-        let actual_token_hash = hex::encode(token.token_hash);
-        let expected_token_hash = "098f6bcd4621d373cade4e832627b4f6";
+        assert_eq!(token_hex, hex::encode(token.token_bytes));
+
+        let actual_token_hash = hex::encode(token.token_md5);
+        let expected_token_hash = "4b579cef88edea8530fb8d00e4f8957e";
         println!("Actual token hash: {actual_token_hash}");
         assert_eq!(expected_token_hash, actual_token_hash);
 
         let actual_iv = hex::encode(token.iv);
-        let expected_iv = "0a9172716ae6428409885b8b829ccb05";
+        let expected_iv = "da0980c978339c0e4610ed1498ba4db2";
         println!("Actual iv: {actual_iv}");
         assert_eq!(expected_iv, actual_iv);
     }
@@ -55,10 +65,14 @@ mod tests {
     fn test_token_encryption() {
         let payload = "Hello world";
         let token = MiIoToken::new("586e584268475142564d485234734d4b").unwrap();
+
         let encrypted = token.encrypt(&payload.as_bytes()).unwrap();
-        let actual_encrypted_hex = hex::encode(encrypted);
-        let expected_encrypted_hex = "098f6bcd4621d373cade4e832627b4f6";
+        let actual_encrypted_hex = hex::encode(&encrypted);
+        let expected_encrypted_hex = "00ddf4342e3a4f49984204a2eceefeef";
         println!("Actual encrypted hex: {actual_encrypted_hex}");
         assert_eq!(expected_encrypted_hex, actual_encrypted_hex);
+
+        let decrypted = token.decrypt(encrypted.deref()).unwrap();
+        assert_eq!(payload.as_bytes(), decrypted.deref())
     }
 }
